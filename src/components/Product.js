bc514abb4a6968658512c0dfdfd243028fa5ade1
chandru90 <iochandru90@gmail.com>
2024-12-25 10:06:40 +0530

    commit 5

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,7 +13,7 @@ const Product = ({ product }) => {
     <div className="product">
       <img src={product.image} alt={product.title} />
       <h3>{product.title}</h3>
-      <p>${product.price}</p>
+      <p>Rs.{product.price}</p>
       <button onClick={handleAddToCart}>Add to Cart</button>
     </div>
   );
